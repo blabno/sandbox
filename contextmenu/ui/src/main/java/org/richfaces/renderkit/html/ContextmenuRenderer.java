74bfa617f6359a57e85f2fe9d7a39e3176fc8c44
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-04-02 12:27:21 +0200

    Change dependency from jquery-ui-core.js to jquery.ui.core.js

--- contextmenu/ui/src/main/java/org/richfaces/renderkit/html/ContextmenuRenderer.java
+++ contextmenu/ui/src/main/java/org/richfaces/renderkit/html/ContextmenuRenderer.java
@@ -43,7 +43,7 @@ import java.util.Map;
 @ResourceDependencies({@ResourceDependency(library = "javax.faces", name = "jsf.js"), @ResourceDependency(name = "jquery.js", target = "head"),
     @ResourceDependency(name = "richfaces.js", target = "head"), @ResourceDependency(name = "richfaces-base-component.js", target = "head"),
     @ResourceDependency(name = "richfaces-event.js", target = "head"),
-    @ResourceDependency(library = "org.richfaces", name = "jquery-ui-core.js", target = "head"),
+    @ResourceDependency(library = "com.jqueryui", name = "jquery.ui.core.js", target = "head"),
     @ResourceDependency(name = "jquery.contextMenu.js", target = "head"), @ResourceDependency(name = "richfaces.contextmenu.js", target = "head"),
     @ResourceDependency(name = "contextmenu.css", target = "head")})
 public class ContextmenuRenderer extends RendererBase {
